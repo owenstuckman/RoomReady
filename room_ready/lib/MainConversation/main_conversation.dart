c1797134c7315658409f540328aec531725301ed
owenstuckman <ostuckman@outlook.com>
2024-10-20 14:52:22 -0400

    fixed

--- room_ready/lib/MainConversation/main_conversation.dart
+++ room_ready/lib/MainConversation/main_conversation.dart
@@ -1,20 +1,20 @@
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-// import 'conversation_helpers/message_input_field.dart';
-// import 'conversation_helpers/message_list.dart';
+import 'conversation_helpers/message_input_field.dart';
+import 'conversation_helpers/message_list.dart';
 
-// class ChatScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text('Conversation')),
-//       body: Column(
-//         children: [
-//           Expanded(child: MessageListScreen()),
-//           MessageInputField(),
-//         ],
-//       ),
-//     );
-//   }
-// }
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Conversation')),
+      body: Column(
+        children: [
+          Expanded(child: MessageListScreen()),
+          MessageInputField(),
+        ],
+      ),
+    );
+  }
+}
