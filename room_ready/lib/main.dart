e6246e6b37b59fdbc5938c6926b2c69f333a0ffb
owenstuckman <ostuckman@outlook.com>
2024-10-20 01:06:46 -0400

    supabase init

--- room_ready/lib/main.dart
+++ room_ready/lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:room_ready/Startup/loading_page.dart';
 import 'package:room_ready/themes.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://tsewlrukrykkycootlsb.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzZXdscnVrcnlra3ljb290bHNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkzNjcwNTUsImV4cCI6MjA0NDk0MzA1NX0.DNwdRirUSViojOqOOLHaJ_lW9u2jd1siOxqdmQWz6PU',
+  );
+  runApp(MyApp());
 }
+
+// Get a reference your Supabase client
+final supabase = Supabase.instance.client;
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
